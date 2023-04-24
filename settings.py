6356b3924938ef23d6d0d0dcde8a345e48e2f321
Keino Chichester <keino.chichester@gmail.com>
2023-04-24 17:37:57 -0400

    user tweaks

--- settings.py
+++ settings.py
@@ -3,4 +3,21 @@ DATABASES = {
         'ENGINE': 'djongo',
         'NAME': 'G249database',
     }
-}
+},
+INSTALLED_APPS = [
+    # ...
+    'corsheaders',
+    # ...
+],
+MIDDLEWARE = [
+    # ...
+    'corsheaders.middleware.CorsMiddleware',
+    # ...
+],
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = [
+    # Add the domains or origins that are allowed to make CORS requests
+    'http://localhost:8000',  # Example: Allow requests from localhost:3000
+    'g249-django-git-main-keinoc.vercel.app',
+    "https://g249-app.herokuapp.com"   # Example: Allow requests from example.com
+]
