aece527eb21a47979ebf8b8fdbaf3a0b765d68c7
Keino Chichester <keino.chichester@gmail.com>
2023-04-24 17:16:06 -0400

    user tweaks

--- urls.py
+++ urls.py
@@ -3,14 +3,7 @@ from .views import my_view_function, users, admin
 
 urlpatterns = [
     path('my-url/', my_view_function, name='my-view-name'),
-]
-
-
-urlpatterns = [
     path('users/', users, name='users'),
-]
-
-urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django249.urls')),
 ]
