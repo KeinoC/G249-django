37daf0a4e0d0ed79f516cb6dd7bf86cc8b8a103d
Keino Chichester <keino.chichester@gmail.com>
2023-04-24 17:20:18 -0400

    user tweaks

--- urls.py
+++ urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('my-url/', my_view_function, name='my-view-name'),
     path('users/', users, name='users'),
     path('admin/', admin.site.urls),
-    path('', include('django249.urls')),
+    # Include any other URL patterns for your project here
 ]
